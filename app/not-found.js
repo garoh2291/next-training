a4c9fdc8bea94b85df529832215bfa7cd96534c9
garoh2291 <garoh2291@gmail.com>
2024-02-26 15:22:19 +0400

    first commit

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Sorry, we couldnt find what you were looking for.</p>
+    </main>
+  );
+}
---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "garnik-nextjs-demo-users-image.s3.eu-north-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+module.exports = nextConfig;
